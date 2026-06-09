c4f7ba5ebc0bc9c13662e7c1b2d5aac213099c49
appbuilt <ankitasarmah91@gmail.com>
2026-06-09 12:03:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'fairway-v5';
-const ASSETS = ['/', '/index.html', '/app.js', '/icon-192.png', '/icon-512.png',
+const CACHE = 'fairway-v6';
+const ASSETS = ['/', './index.html', '/app.js', '/icon-192.png', '/icon-512.png',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,600;0,700;1,400&family=Source+Sans+3:wght@300;400;500;600&display=swap'
